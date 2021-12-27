4389313bc7b26131884e29f97db7a9e19c901bbc
scherbich211 <80286771+scherbich211@users.noreply.github.com>
2021-12-27 18:42:48 +0300

    App.js => App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import { Provider } from 'react-redux';
-// import {store} from './App/Redux/store/store';
-
 import Navigator from './App/Redux/Navigator';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
@@ -10,7 +7,7 @@ import persistStoreFor from './App/Redux/store/store';
 
 let persisted = persistStoreFor();
 
-function App() {
+const App = (): JSX.Element => {
 	return (
 		<Provider store={persisted.store}>
 			<PersistGate persistor={persisted.persistor} loading={null}>
@@ -21,16 +18,3 @@ function App() {
 }
 
 export default App;
-
-// export default class App extends Component {
-// 	componentDidMount() {
-// 		Permission.checkPermission(PERMISSION_TYPE.camera);
-// 	}
-// 	render() {
-// 		return (
-// 			<View>
-// 				<Text> react permission</Text>
-// 			</View>
-// 		);
-// 	}
-// }
